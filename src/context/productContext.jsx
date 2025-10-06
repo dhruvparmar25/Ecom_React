8a22f96e8a4c1c124bbdf3a0af9719d08c05a0c8
dhruvparmar25 <parmardhruv034@gmail.com>
2025-10-06 12:28:54 +0530

    #13 Fetch Api

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -1,16 +1,26 @@
-import { createContext, useContext } from "react"
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
-
+const API = "http://localhost:3000/api/product";
 const AppProvider = ({ children }) => {
-    return (<AppContext.Provider value={{ myname: "DHruv PArmar" }}>
-        {children}
-    </AppContext.Provider>)
+  const getProducts = async (url) => {
+    const res = await axios.get(url);
+    const products = await res.data.data;
+    console.log("🚀 ~ getProducts ~ products:", products);
+  };
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+  return (
+    <AppContext.Provider value={{ myname: "DHruv PArmar" }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // Custom Hooks
 const useProductContext = () => {
-    return useContext(AppContext);
-
-}
+  return useContext(AppContext);
+};
 // eslint-disable-next-line react-refresh/only-export-components
-export { AppProvider, AppContext, useProductContext } 
+export { AppProvider, AppContext, useProductContext };
